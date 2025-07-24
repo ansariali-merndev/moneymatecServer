c478c275d948b811f214d79eb881a07e31823cdf
ansariali <=>
2025-07-24 23:51:06 +0530

    fixing cookie problem

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -92,6 +92,7 @@ export const verifyEmail = async (req, res) => {
       httpOnly: true,
       path: "/",
       maxAge: 24 * 60 * 60 * 1000,
+      sameSite: "none",
       secure: true,
     });
 
@@ -166,6 +167,7 @@ export const login = async (req, res) => {
       httpOnly: true,
       path: "/",
       maxAge: 24 * 60 * 60 * 1000,
+      sameSite: "none",
       secure: true,
     });
 
